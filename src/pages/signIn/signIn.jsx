1cde75c6c935cf246e0813a4bc0064d6f29790b5
ukssss <1872003115su@gmail.com>
2023-06-12 16:07:15 +0900

    [feat] SignIn CSS 1차 수정
    - styled-components 사용
    - 개발을 위한 시인성 향상을 목적으로 일부만 수정, 추후 CSS 수정 예정

--- src/pages/signIn/signIn.jsx
+++ src/pages/signIn/signIn.jsx
@@ -1,21 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 export default function SignIn() {
+    const [id, setID] = useState('');
+    const [password, setPW] = useState('');
+
+    const onChangeID = () => {};
+    const onChangePW = () => {};
+    const onSubmit = () => {};
+
     return (
-        <>
-            <form method="post" id="signin-form">
-                <input type="text" name="userID" placeholder="ID" />
-                <input
+        <LoginWrapper>
+            <MyH2>로그인 페이지</MyH2>
+            <LoginForm method="post" id="signin-form">
+                <LoginInput
+                    type="text"
+                    name="userID"
+                    placeholder="ID"
+                    onChange={onChangeID}
+                />
+                <LoginInput
                     type="password"
                     name="userPassword"
                     placeholder="Password"
+                    onChange={onChangePW}
                 />
-                <label htmlFor="remember">
-                    <input type="checkbox" id="remember" />
+                <LoginLabel htmlFor="remember">
+                    <LoginCheckbox type="checkbox" id="remember" />
                     아이디 저장하기
-                </label>
-                <input type="submit" value="Login" />
-            </form>
-        </>
+                </LoginLabel>
+                <LoginSubmit type="submit" value="Login" onClick={onSubmit} />
+            </LoginForm>
+        </LoginWrapper>
     );
 }
+
+const LoginWrapper = styled.div`
+    width: 400px;
+    height: 350px;
+    padding: 40px;
+    box-sizing: border-box;
+`;
+
+const MyH2 = styled.h2`
+    font-size: 24px;
+    color: #6a24fe;
+    margin-bottom: 20px;
+`;
+
+const LoginForm = styled.form``;
+
+const LoginInput = styled.input`
+    width: 100%;
+    height: 48px;
+    padding: 0 10px;
+    box-sizing: border-box;
+    margin-bottom: 16px;
+    border-radius: 6px;
+    background-color: #f8f8f8;
+`;
+
+const LoginLabel = styled.label`
+    color: #999999;
+    padding-left: 26px;
+    padding-right: 26px;
+`;
+
+const LoginCheckbox = styled.input``;
+
+const LoginSubmit = styled.input`
+    color: #fff;
+    font-size: 16px;
+    background-color: #6a24fe;
+    margin-top: 20px;
+`;
