8ffe70f8b5f8db033cdeedb826d1c3369abf93ed
ukssss <1872003115su@gmail.com>
2023-06-12 14:44:08 +0900

    [feat] SignIn 마크업 완성

--- src/pages/signIn/signIn.jsx
+++ src/pages/signIn/signIn.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default function SignIn() {
-    return <div>로그인 기능 개발</div>;
+    return (
+        <>
+            <form method="post" id="signin-form">
+                <input type="text" name="userID" placeholder="ID" />
+                <input
+                    type="password"
+                    name="userPassword"
+                    placeholder="Password"
+                />
+                <label htmlFor="remember">
+                    <input type="checkbox" id="remember" />
+                    아이디 저장하기
+                </label>
+                <input type="submit" value="Login" />
+            </form>
+        </>
+    );
 }
