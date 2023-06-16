e7469a2e6a283cd4884f83f853b00795655df64c
ukssss <1872003115su@gmail.com>
2023-06-16 18:33:32 +0900

    [FEAT] todo 를 추가하면 즉시 todolist 출력 구현
    - axios.get

--- src/pages/todo/todo.jsx
+++ src/pages/todo/todo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -6,23 +6,39 @@ import TodoForm from '../../components/todo/todoForm/todoForm';
 import TodoUl from '../../components/todo/todoUl/todoUl';
 import TodoList from '../../components/todo/todoList/todoList';
 
+import axios from 'axios';
+
 export default function Todo() {
     const navigate = useNavigate();
-    const isLoggedIn = localStorage.getItem('token');
+    const access_token = localStorage.getItem('token');
 
     useEffect(() => {
-        if (!isLoggedIn) {
+        if (!access_token) {
             navigate('/signin');
         }
-    }, [navigate, isLoggedIn]);
+    }, [navigate, access_token]);
+
+    const [todo, setTodo] = useState([]);
+
+    const getPosts = async () => {
+        const posts = await axios.get(`http://localhost:8000/todos`, {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        setTodo(posts.data);
+    };
+    getPosts();
 
     return (
         <TodoWrapper>
             <MyH2>Todo List</MyH2>
             <TodoForm />
             <TodoUl>
-                <TodoList>Todo 1</TodoList>
-                <TodoList>Todo 2</TodoList>
+                {todo.map((el) => {
+                    return <TodoList>{el.todo}</TodoList>;
+                })}
             </TodoUl>
         </TodoWrapper>
     );
