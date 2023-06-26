75922f6e3399a153112a1da4832fb2cfdd0bbe7d
ukssss <1872003115su@gmail.com>
2023-06-26 21:56:39 +0900

    [REFACTOR] todo 관련 초기화
    - todoList context api 적용 관련 학습을 위한 초기화 적용

--- src/pages/todo/todo.jsx
+++ src/pages/todo/todo.jsx
@@ -1,34 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import styled from 'styled-components';
 
-import { TodoProvider } from '../../context/todoContext/todoContext';
-
-import TodoHead from '../../components/todo/todoHead/todoHead';
-import TodoList from '../../components/todo/todoList/todoList';
-import TodoCreate from '../../components/todo/todoCreate/todoCreate';
-
 export default function Todo() {
-    const navigate = useNavigate();
-    const access_token = localStorage.getItem('token');
-
-    useEffect(() => {
-        if (!access_token) {
-            navigate('/signin');
-        }
-    }, [navigate, access_token]);
-
-    return (
-        <TodoProvider>
-            <MyH2>Todo List</MyH2>
-            <TodoWrapper>
-                <TodoHead />
-                <TodoCreate />
-                <TodoList />
-            </TodoWrapper>
-        </TodoProvider>
-    );
+    return <div>TODO</div>;
 }
-
-const TodoWrapper = styled.div``;
-const MyH2 = styled.h2``;
