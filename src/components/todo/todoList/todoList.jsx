05a0c9616918be74eb0cf78ae19abdbc05268c34
ukssss <1872003115su@gmail.com>
2023-06-28 23:29:58 +0900

    [FEAT] axios 통신, get 구현
    - 토큰 조회 후 저장된 todolist 출력

--- src/components/todo/todoList/todoList.jsx
+++ src/components/todo/todoList/todoList.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import { DEV_ADDRESS } from '../../../api/api';
 
 import TodoItem from '../todoItem/todoItem';
+
 import { useTodoState } from '../../../context/todoContext';
 
+const access_token = localStorage.getItem('token');
+const url = DEV_ADDRESS;
+const api = axios.create({
+    baseURL: url,
+    headers: {
+        Authorization: `Bearer ${access_token}`,
+        'Content-Type': `application/json`,
+    },
+});
+
 export default function TodoList() {
-    const todos = useTodoState();
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    const getTodos = async () => {
+        try {
+            const res = await api.get('/todos');
+            setTodos(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <List>
-            {todos.map((todo) => (
-                <TodoItem
-                    key={todo.id}
-                    id={todo.id}
-                    checked={todo.done}
-                    text={todo.text}
-                />
-            ))}
+            {todos &&
+                todos.map((todo) => (
+                    <TodoItem
+                        key={todo.id}
+                        id={todo.id}
+                        checked={todo.isCompleted}
+                        text={todo.todo}
+                    />
+                ))}
         </List>
     );
 }
