d34025b5a6beac636411c1eb2d2a3fa30cea5eba
ukssss <1872003115su@gmail.com>
2023-06-30 10:56:20 +0900

    [FEAT] todo 추가 기능 구현
    - 추가 button을 클릭 혹은 Enter 할 경우 입력 input의 내용이 새로운 todo로 추가
    - todo 추가하면 서버로 해당 todo가 추가, 즉 새로고침해도 추가한 todo가 목록에 보이도록 구현

--- src/components/todo/todoCreate/todoCreate.jsx
+++ src/components/todo/todoCreate/todoCreate.jsx
@@ -4,29 +4,19 @@ import styled from 'styled-components';
 import InputText from '../../input/inputText';
 import Button from '../../button/button';
 
-import { useTodoDispatch, useTodoNextId } from '../../../context/todoContext';
-
-function TodoCreate() {
+function TodoCreate({ createTodo }) {
     const [value, setValue] = useState('');
-
-    const dispatch = useTodoDispatch();
-    const nextId = useTodoNextId();
 
     const onChange = (e) => setValue(e.target.value);
     const onSubmit = (e) => {
         e.preventDefault();
 
-        dispatch({
-            type: 'CREATE',
-            todo: {
-                id: nextId.current,
-                text: value,
-                done: false,
-            },
-        });
+        const todoData = {
+            todo: value,
+        };
 
+        createTodo(todoData);
         setValue('');
-        nextId.current += 1;
     };
 
     return (
@@ -36,8 +26,9 @@ function TodoCreate() {
                 placeholder="할 일을 입력 후, Enter 를 누르세요"
                 onChange={onChange}
                 value={value}
+                data-testid="new-todo-input"
             />
-            <Button type="button" onClick={onSubmit}>
+            <Button onClick={onSubmit} data-testid="new-todo-add-button">
                 추가
             </Button>
         </TodoForm>
--- src/pages/todo/todo.jsx
+++ src/pages/todo/todo.jsx
@@ -1,11 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import axios from 'axios';
+import { DEV_ADDRESS } from '../../api/api';
 
 import TodoCreate from '../../components/todo/todoCreate/todoCreate';
 import TodoList from '../../components/todo/todoList/todoList';
 import TodoTemplate from '../../components/todo/todoTemplate/todoTemplate';
-import { TodoProvider } from '../../context/todoContext';
+import TodoItem from '../../components/todo/todoItem/todoItem';
+
+import { TodoProvider, useTodoState } from '../../context/todoContext';
+
+const access_token = localStorage.getItem('token');
+const url = DEV_ADDRESS;
+const api = axios.create({
+    baseURL: url,
+    headers: {
+        Authorization: `Bearer ${access_token}`,
+        'Content-Type': `application/json`,
+    },
+});
 
 export default function Todo() {
     const navigate = useNavigate();
@@ -17,12 +31,47 @@ export default function Todo() {
         }
     }, [navigate, access_token]);
 
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    const getTodos = async () => {
+        try {
+            const res = await api.get('/todos');
+            setTodos(res.data);
+            console.log(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const createTodo = async (todoData) => {
+        try {
+            const res = await api.post('/todos', todoData);
+            getTodos(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <TodoProvider>
             <TodoTemplate>
                 <MyH2>Todo List</MyH2>
-                <TodoCreate />
-                <TodoList />
+                <TodoCreate createTodo={createTodo} />
+                <TodoList>
+                    {todos &&
+                        todos.map((todo) => (
+                            <TodoItem
+                                key={todo.id}
+                                id={todo.id}
+                                checked={todo.isCompleted}
+                                text={todo.todo}
+                            />
+                        ))}
+                </TodoList>
             </TodoTemplate>
         </TodoProvider>
     );
